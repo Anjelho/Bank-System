2e21795600c337ec0aa226ad30c908b1ad4664d6
Ivaylo.Lambrev <acta90@gmail.com>
2020-02-04 17:46:33 +0200

    new account system
    Signed-off-by: Ivaylo.Lambrev <acta90@gmail.com>

--- main.py
+++ main.py
@@ -1,233 +1,176 @@
-import pickle
-import os
-import pathlib
-from datetime import date
+from datetime import datetime
 from datetime import timedelta
+import psycopg2 as pg
+import json
+from decimal import *
+
+def connect_to_postgres():
+    """Returns a connection to the database.
+
+    TODO: Remove hardcoded connection string
+    """
+    with open("config/config.json") as json_config:
+        cfg = json.load(json_config)
+
+    return pg.connect(cfg['localdb'])
+
+
+class BalanceError(Exception):
+    value = "Sorry you have only $%6.2f in your account"
 
 
 class Account:
+    """represents type of bank account """
 
-    def __init__(self, accNo=0, date=date.today(), deposit=0, type='', termdate=''):
-        self.accNo = accNo
-        self.date = date
-        self.deposit = deposit
-        self.type = type
-        self.termdate = termdate
+    def __init__(self, customer_name: str, opening_date, account_number, balance=0):
+        """ Constructor for bank account """
+        self.customer_name = customer_name
+        self.opening_date = datetime.strptime(opening_date, "%Y-%m-%d").date()
+        self.account_number = account_number
+        self.balance = balance
 
-    def createAccount(self):
-        x = ''
-        self.accNo = int(input("Enter the account no : "))
-        while x != "C" or x != "D":
-            x = input('Please specify the account type [C/D]: ')
-            if x == "C":
-                self.type = x
-                break
-            elif x == "D":
-                self.type = x
-                self.termdate = date.today() + timedelta(days=365)
-                break
-        self.deposit = int(input("Enter The Initial amount : "))
-        self.date = date.today()
-        print("\n\n\nAccount Created")
+    @staticmethod
+    def get_current_date():
+        date = datetime.today().date()
+        return date
 
-    def showAccount(self):
-        print("Account Number : ", self.accNo)
-        print("Type of Account : ", self.type)
-        print("Term date : ", self.termdate)
-        print("Balance : ", self.deposit)
+    def deposit(self, deposit_amount):
+        """ modifier/mutator to deposit to account """
 
-    def depositAmount(self, amount):
-        self.deposit += amount
+        self.balance = self.balance + deposit_amount
+        return self.balance
 
-    def withdrawAmount(self, amount):
-        self.deposit -= amount
+    def withdraw(self, withdraw_amount):
+        """ modifier/mutator to withdraw from account """
+        if self.balance >= withdraw_amount:
+            self.balance = self.balance - withdraw_amount
+        else:
+            raise BalanceError(BalanceError.value % self.balance)
 
-    def report(self):
-        print(self.accNo, " ", self.type, " ", self.deposit, " ", self.date, " ", self.termdate)
+    def transfer(self, account, amount: float):
+        try:
+            self.withdraw(amount)
+            account.deposit(amount)
+        except BalanceError:
+            print(BalanceError.value % self.balance)
 
-    def getAccountNo(self):
-        return self.accNo
+    def get_balance(self):
+        """ Accessor method for balance """
+        return self.balance
 
-    def getAccountType(self):
-        return self.type
+    def set_balance(self, new_balance):
+        """ modifier/mutator method for balance """
 
-    def getDeposit(self):
-        return self.deposit
+        self.balance = new_balance
 
-
-def intro():
-    print("\t\t\t\t**********************")
-    print("\t\t\t\tBANK MANAGEMENT SYSTEM")
-    print("\t\t\t\t**********************")
-
-    print("\t\t\t\tBrought To You By:")
-    print("\t\t\t\tprojectworlds.in")
-    input()
+    def display(self):
+        """ helper/support method to display account info """
+        print("Customer name:", self.customer_name)
+        print("Opening date:", self.opening_date)
+        print("Balance: ${0:.2f}".format(self.balance))
 
 
-def writeAccount():
-    account = Account()
-    account.createAccount()
-    writeAccountsFile(account)
+class CurrentAccount(Account):
+    """ subclass for current account """
+
+    def __init__(self, customer_name, opening_date, account_number, balance):
+        """ constructor for current account """
+        self.interest = 0
+        self.account_type = 'Current Account'
+
+        if datetime.strptime(opening_date, "%Y-%m-%d").date() < datetime.today().date():
+            raise ValueError('Opening date cannot be earlier than Current day')
+        elif account_number < 0 or balance < 0:
+            raise ValueError('Account number or balance cannot be negative')
+        else:
+            super().__init__(customer_name, opening_date, account_number, balance)
+
+    def display_monthly_statement(self):
+        print("Current Account Monthly Statement")
+        super().display()
+
+    def display(self):
+        super().display()
+        print("Account No:", self.account_number)
+        print("Account type:", self.account_type)
+        print("Interest:", self.interest)
+
+    def get_rate_of_interest(self):
+        return self.interest
 
 
-def displayAll():
-    file = pathlib.Path("accounts.data")
-    if file.exists():
-        infile = open('accounts.data', 'rb')
-        mylist = pickle.load(infile)
-        for item in mylist:
-            print(item.accNo, " ", item.type, " ", item.deposit, " ", item.date, " ", item.termdate)
-        infile.close()
-    else:
-        print("No records to display")
+class DepositAccount(Account):  # inheritance
+    """ subclass for deposit account """
+
+    def __init__(self, customer_name, opening_date, account_number, term_date, interest: float, balance):
+        """ constructor for Deposit account
+         :param interest:
+                percentage
+         :param opening_date:
+                :returns datetime.date
+        :param term_date:
+               :returns datetime.date"""
+
+        self.account_type = 'Deposit Account'
+        self.interest = interest
+        self.term_date = datetime.strptime(term_date, "%Y-%m-%d").date()
+
+        if datetime.strptime(term_date, "%Y-%m-%d").date() < datetime.today().date():
+            raise ValueError('Term date cannot be earlier than Current day')
+
+        elif datetime.strptime(opening_date, "%Y-%m-%d").date() < datetime.today().date():
+            raise ValueError('Opening date cannot be earlier than Current day')
+
+        elif account_number < 0 or balance < 0:
+            raise ValueError('Account number or balance cannot be negative')
+
+        else:
+            super().__init__(customer_name, opening_date, account_number, balance)
+
+    def get_term_date(self):
+        return self.term_date.strftime("%Y-%m-%d")
+
+    def set_term_date(self, value):
+        self.term_date = datetime.strptime(value, "%Y-%m-%d").date()
+
+    def get_rate_of_interest(self):
+        return self.interest
+
+    def display(self):
+        super().display()
+        print("Account No:", self.account_number)
+        print("Account type:", self.account_type)
+        print("Term date:", self.term_date)
+        print("Interest: {}%".format(self.interest))
+
+    def display_yearly_statement(self):
+        self.set_balance(self.get_balance() * (1 + self.interest / 100))
+        print("Deposit Account yearly Statement")
+        super().display()
+
+    def display_term_date(self):
+        print('Current term date is: {}'.format(self.term_date))
+
+    def withdraw(self, withdraw_amount):
+        """ modifier/mutator to withdraw from account """
+
+        if self.term_date == Account.get_current_date():
+            return Account.withdraw(self, withdraw_amount)
+        else:
+            raise ValueError("You can withdraw only on term date!")
+
+    def transfer(self, account, amount):
+        if self.term_date == Account.get_current_date():
+            return Account.transfer(self, account, amount)
+        else:
+            raise ValueError("You can transfer only on term date!")
 
 
-def displaySp(num):
-    file = pathlib.Path("accounts.data")
-    if file.exists():
-        infile = open('accounts.data', 'rb')
-        mylist = pickle.load(infile)
-        infile.close()
-        found = False
-        for item in mylist:
-            if item.accNo == num:
-                print("Your account Balance is = ", item.deposit)
-                found = True
-    else:
-        print("No records to Search")
-    if not found:
-        print("No existing record with this number")
+d = DepositAccount('Shawn','2020-02-04', 1, '2021-02-03',0.1,1000)
+c = CurrentAccount('Clown','2020-02-05', -1, 1000)
 
 
-def depositAndWithdraw(num1, num2):
-    file = pathlib.Path("accounts.data")
-    if file.exists():
-        infile = open('accounts.data', 'rb')
-        mylist = pickle.load(infile)
-        infile.close()
-        os.remove('accounts.data')
-        for item in mylist:
-            if item.accNo == num1:
-                if num2 == 1:
-                    amount = int(input("Enter the amount to deposit : "))
-                    item.deposit += amount
-                    print("Your account is updated")
-                elif num2 == 2:
-                    amount = int(input("Enter the amount to withdraw : "))
-                    if amount <= item.deposit:
-                        item.deposit -= amount
-                    else:
-                        print("You cannot withdraw larger amount!")
-
-    else:
-        print("No records to Search")
-    outfile = open('newaccounts.data', 'wb')
-    pickle.dump(mylist, outfile)
-    outfile.close()
-    os.rename('newaccounts.data', 'accounts.data')
-
-
-def deleteAccount(num):
-    file = pathlib.Path("accounts.data")
-    if file.exists():
-        infile = open('accounts.data', 'rb')
-        oldlist = pickle.load(infile)
-        infile.close()
-        newlist = []
-        for item in oldlist:
-            if item.accNo != num:
-                newlist.append(item)
-        os.remove('accounts.data')
-        outfile = open('newaccounts.data', 'wb')
-        pickle.dump(newlist, outfile)
-        outfile.close()
-        os.rename('newaccounts.data', 'accounts.data')
-
-
-def modifyAccount(num):
-    file = pathlib.Path("accounts.data")
-    if file.exists():
-        infile = open('accounts.data', 'rb')
-        oldlist = pickle.load(infile)
-        infile.close()
-        os.remove('accounts.data')
-        for item in oldlist:
-            if item.accNo == num:
-                while item.type != "C" or item.type != "D":
-                    item.type = input('Please specify the account type [C/D]: ')
-                    if item.type == "C":
-                        item.termdate = ''
-                        break
-                    elif item.type == "D":
-                        item.termdate = item.date + timedelta(days=365)
-                        break
-                item.deposit = int(input("Enter the Amount : "))
-
-        outfile = open('newaccounts.data', 'wb')
-        pickle.dump(oldlist, outfile)
-        outfile.close()
-        os.rename('newaccounts.data', 'accounts.data')
-
-
-def writeAccountsFile(account):
-    file = pathlib.Path("accounts.data")
-    if file.exists():
-        infile = open('accounts.data', 'rb')
-        oldlist = pickle.load(infile)
-        oldlist.append(account)
-        infile.close()
-        os.remove('accounts.data')
-    else:
-        oldlist = [account]
-    outfile = open('newaccounts.data', 'wb')
-    pickle.dump(oldlist, outfile)
-    outfile.close()
-    os.rename('newaccounts.data', 'accounts.data')
-
-# start of the program
-ch = ''
-num = 0
-intro()
-
-while ch != 8:
-    # system("cls");
-    print("\tMAIN MENU")
-    print("\t1. NEW ACCOUNT")
-    print("\t2. DEPOSIT AMOUNT")
-    print("\t3. WITHDRAW AMOUNT")
-    print("\t4. BALANCE ENQUIRY")
-    print("\t5. ALL ACCOUNT HOLDER LIST")
-    print("\t6. CLOSE AN ACCOUNT")
-    print("\t7. MODIFY AN ACCOUNT")
-    print("\t8. EXIT")
-    print("\tSelect Your Option (1-8) ")
-    ch = input()
-    # system("cls");
-
-    if ch == '1':
-        writeAccount()
-    elif ch == '2':
-        num = int(input("\tEnter The account No. : "))
-        depositAndWithdraw(num, 1)
-    elif ch == '3':
-        num = int(input("\tEnter The account No. : "))
-        depositAndWithdraw(num, 2)
-    elif ch == '4':
-        num = int(input("\tEnter The account No. : "))
-        displaySp(num)
-    elif ch == '5':
-        displayAll();
-    elif ch == '6':
-        num = int(input("\tEnter The account No. : "))
-        deleteAccount(num)
-    elif ch == '7':
-        num = int(input("\tEnter The account No. : "))
-        modifyAccount(num)
-    elif ch == '8':
-        print("\tThanks for using bank managemnt system")
-        break
-    else:
-        print("Invalid choice")
-
-    ch = input("Press 'Enter' to Continue\t")
+d.display()
+#d.display_yearly_statement()
+#d.transfer(c,100)
+#c.display()

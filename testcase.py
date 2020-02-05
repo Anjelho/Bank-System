7b797fd42fce3e3bab2289989e85815d7f3ea64b
Anjelho <angel.kostadinow@gmail.com>
2020-02-05 10:28:59 +0200

    new version pre-final

--- testcase.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-import numpy as np
-history = 'history.json'
-with open(history, 'r') as infile:
-    hist = json.load(infile)
-    df = pd.DataFrame(hist).transpose()
-    print(df)
---
+++ testdate.py
@@ -0,0 +1,15 @@
+import bank
+import datetime
+#difference = bank.date.today() - datetime.date(2020, 2, 1)
+#print(int(difference))
+
+from datetime import datetime as dtm
+
+def days_between(d1, d2):
+    d1 = dtm.strptime(d1, "%Y-%m-%d")
+    d2 = dtm.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
+
+d = datetime.date(2019, 2 ,5)
+x=days_between(str(bank.date.today()), str(datetime.date(2020, 2 , 1)))
+print(x)
